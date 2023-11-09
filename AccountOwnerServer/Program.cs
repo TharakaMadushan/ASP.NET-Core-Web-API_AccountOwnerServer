fa02c09b69bfe925f6e3fd1c5e8cc49a5c26168f
Tharaka Madushan <mtharaka37@gmail.com>
2023-11-09 23:14:18 +0530

    Add project files.

---
+++ AccountOwnerServer/Program.cs
@@ -0,0 +1,41 @@
+using AccountOwnerServer.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,23 @@
+﻿using Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+        private static ILogger logger = LogManager.GetCurrentClassLogger();
+        void ILoggerManager.LogDebug(string message) => logger.Debug(message);
+
+        void ILoggerManager.LogError(string message) => logger.Error(message);
+
+        void ILoggerManager.LogInfo(string message) => logger.Info(message);
+
+        void ILoggerManager.LogWarn(string message) => logger.Warn(message);
+
+    }
+}
